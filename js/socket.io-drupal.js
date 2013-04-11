3e654ae77bedc13ed7a7c37d3f6ed52e87c37035
Bas van der Heijden <bas.van.der.heijden@dop.nu>
2013-04-11 15:17:45 +0200

    Moved Drupal.io higher up the chain so it loads earlier

--- js/socket.io-drupal.js
+++ js/socket.io-drupal.js
@@ -1,33 +1,31 @@
 (function($) {
-  Drupal.behaviors.socket_io = {
-    attach: function(context, settings) {
-      Drupal.socket_connected = false;
+  $(function() {
+    Drupal.socket_connected = false;
 
-      if (typeof io === 'undefined') {
-        // Socket.io was not succesfully loaded.
-        return;
-      }
-
-      Drupal.io = io;
-
-      Drupal.socket_connect = function(cb) {
-        Drupal.socket = Drupal.io.connect(settings.socket_io.connection_string);
-
-        Drupal.socket.on('connect', function() {
-          Drupal.socket_connected = true;
-          if (typeof cb === 'function') {
-            cb(Drupal.socket);
-          }
-        });  
-
-        Drupal.socket.on('disconnect', function() {
-          Drupal.socket_connected = false;
-        });
-      };
-
-      if (settings.socket_io.auto_connect) {
-        Drupal.socket_connect();
-      }
+    if (typeof io === 'undefined') {
+      // Socket.io was not succesfully loaded.
+      return;
     }
-  };
+
+    Drupal.io = io;
+
+    Drupal.socket_connect = function(cb) {
+      Drupal.socket = Drupal.io.connect(Drupal.settings.socket_io.connection_string);
+
+      Drupal.socket.on('connect', function() {
+        Drupal.socket_connected = true;
+        if (typeof cb === 'function') {
+          cb(Drupal.socket);
+        }
+      });  
+
+      Drupal.socket.on('disconnect', function() {
+        Drupal.socket_connected = false;
+      });
+    };
+
+    if (Drupal.settings.socket_io.auto_connect) {
+      Drupal.socket_connect();
+    }
+  });
 })(jQuery);
